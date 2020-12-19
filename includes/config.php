5877287fbb1aee30d5a4bdea703b69f3dd723c54
Chris Lauer <56165582+CALauer@users.noreply.github.com>
2020-12-19 17:47:18 -0500

    Update config.php
    Removed Personal Data

--- includes/config.php
+++ includes/config.php
@@ -2,15 +2,12 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 
-$db_serverName = 'localhost';
-$db_username = 'root';
-$db_password = '';
-$db_name = 'users';
-
 // $db_serverName = 'localhost';
-// $db_username = 'chrislc9_admin';
-// $db_password = ',6Lc3.T)oD]*';
-// $db_name = 'chrislc9_db1';
+// $db_username = 'root';
+// $db_password = '';
+// $db_name = 'users';
+
+
 
 $conn = mysqli_connect($db_serverName, $db_username, $db_password, $db_name);
 
